8ad00e5d46d8ff5ba4d7e21fc3200eb1d236d234
VenkatVedicharlaa <109144102+VenkatVedicharlaa@users.noreply.github.com>
2022-07-16 14:56:56 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import "./styles.css";
+
+import { useState } from "react";
+
+//react fucntione with fragments
+
+// const App = () => {
+//   return (
+//     <>
+//       <h1>Venkat vedicharla</h1>
+//       <h1>Venkat vedicharla</h1>
+//       <h1>Venkat vedicharla</h1>
+//     </>
+//   );
+// };
+// export default App;
+
+//react fucntions with variable
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Venkat</h1>
+//       <h2>Venkat</h2>
+//     </div>
+//   );
+// };
+// export default App;
+
+// react functions useState
+
+// const App = () => {
+//   const [name, setName] = useState("KrishnamRaj");
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//     </div>
+//   );
+// };
+// export default App;
+
+// usestate update name with button
+
+// const App = () => {
+//   const [name, setName] = useState("venkat");
+
+//   const click = () => {
+//     setName("Krishnam");
+//   };
+
+//   return (
+//     <div>
+//       <h1>{name}</h1>
+//       <button onClick={click}>Click</button>
+//     </div>
+//   );
+// };
+// export default App;
